a8529898d6f6150d4e1446658fdf06ae4d8c9a16
nabishafin <shafin21215@gmail.com>
2024-09-24 00:34:02 +0600

    2nd section js done

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 
 
 // 1st part....
-document.getElementById('noakhali-button').addEventListener('click', function (e) {
-    e.preventDefault;
-    const input = document.getElementById('noakhali-input').value;
-    const inputValue = parseFloat(input)
+document.getElementById('noakhali-button').addEventListener('click', function () {
+
+    // input value by function
+    const inputValue = getInPutNumberValue('noakhali-input')
 
     if (typeof inputValue === 'number' && inputValue > 0) {
-        const amount = document.getElementById('noakhaliAmount').innerHTML
-        const numberAmount = parseFloat(amount)
+
+        // get donation amount by function
+        const numberAmount = getValueInput('noakhaliAmount')
+
         const updateAmount = numberAmount + inputValue;
         document.getElementById('noakhaliAmount').innerText = updateAmount
 
         // step -2
 
-        const myBalence = document.getElementById('mainBalence').innerText
-        const numbeMyBalence = parseFloat(myBalence);
+        // get mainvalence by function
+        const numbeMyBalence = getmainBalence('mainBalence')
 
+        // update value in my balence
         const updatemyBalence = numbeMyBalence - inputValue
 
         document.getElementById('mainBalence').innerText = updatemyBalence
     }
+})
 
+// 2nd Part
+
+document.getElementById('feniHandle').addEventListener('click', function () {
+
+    const inputValue = getInPutNumberValue('feniInputValue')
+
+    if (typeof inputValue === 'number' && inputValue > 0) {
+        const numberAmount = getValueInput('feniDonationAmount')
+
+        const updateAmount = numberAmount + inputValue;
+        document.getElementById('feniDonationAmount').innerText = updateAmount
+
+        const numbeMyBalence = getmainBalence('mainBalence')
+        const updatemyBalence = numbeMyBalence - inputValue;
+        document.getElementById('mainBalence').innerText = updatemyBalence
+    }
 })
---
+++ utility.js
@@ -0,0 +1,20 @@
+// get inputvalue by function
+function getInPutNumberValue(id) {
+    const input = document.getElementById(id).value;
+    const Value = parseFloat(input)
+    return Value
+}
+
+// get  donation amount by function
+function getValueInput(id) {
+    const amount = document.getElementById(id).innerHTML
+    const numberAmount = parseFloat(amount)
+    return numberAmount
+}
+// get mainbalence by function
+
+function getmainBalence(id) {
+    const myBalence = document.getElementById(id).innerText
+    const Balence = parseFloat(myBalence);
+    return Balence
+}
