f27f262c39ebdafdc204a4b3fca5c08f1f69858c
nabishafin <shafin21215@gmail.com>
2024-09-25 07:06:23 +0600

    first commit

--- index.js
+++ index.js
@@ -1,192 +1,184 @@
-
-
 // 1st part....
-document.getElementById('noakhali-button').addEventListener('click', function () {
-
+document
+  .getElementById("noakhali-button")
+  .addEventListener("click", function () {
     // input value by function
-    const inputValue = getInPutNumberValue('noakhali-input')
+    const inputValue = getInPutNumberValue("noakhali-input");
 
-    if (typeof inputValue === 'number' && inputValue > 0) {
+    if (typeof inputValue === "number" && inputValue > 0) {
+      // get donation amount by function
+      const numberAmount = getValueInput("noakhaliAmount");
 
-        // get donation amount by function
-        const numberAmount = getValueInput('noakhaliAmount')
+      const updateAmount = numberAmount + inputValue;
+      document.getElementById("noakhaliAmount").innerText = updateAmount;
 
-        const updateAmount = numberAmount + inputValue;
-        document.getElementById('noakhaliAmount').innerText = updateAmount
+      //modal functionalities
+      const myModal = document.getElementById("myModal");
+      myModal.classList.remove("hidden");
+      const closeModalBtn = document.getElementById("closeModalBtn");
+      closeModalBtn.addEventListener("click", () => {
+        myModal.classList.add("hidden");
+      });
+      // notification
+      //   alert("Your Have Donated for Humankind");
 
-        // notification
-        alert('Your Have Donated for Humankind')
+      // get mainvalence by function
+      const numbeMyBalence = getmainBalence("mainBalence");
 
+      // update value in my balence
+      const updatemyBalence = numbeMyBalence - inputValue;
 
-        // get mainvalence by function
-        const numbeMyBalence = getmainBalence('mainBalence')
+      document.getElementById("mainBalence").innerText = updatemyBalence;
 
-        // update value in my balence
-        const updatemyBalence = numbeMyBalence - inputValue
-
-        document.getElementById('mainBalence').innerText = updatemyBalence
-
-        // add to history
-        const now = new Date();
-        const div = document.createElement('div')
-        div.classList.add('border-2')
-        div.classList.add('p-4')
-        div.classList.add('mt-4')
-        div.innerHTML =
-            ` ${updateAmount} Taka is Donated at Noakhali, Bangladesh.
+      // add to history
+      const now = new Date();
+      const div = document.createElement("div");
+      div.classList.add("border-2");
+      div.classList.add("p-4");
+      div.classList.add("mt-4");
+      div.innerHTML = ` ${updateAmount} Taka is Donated at Noakhali, Bangladesh.
             <br>
             <p>${now}</p>
-        `
-        const historyDiv = AddedHistory("hidtory")
-        historyDiv.appendChild(div)
+        `;
+      const historyDiv = AddedHistory("hidtory");
+      historyDiv.appendChild(div);
     }
-})
-
+  });
 
 // 2nd Part
-document.getElementById('feniHandle').addEventListener('click', function () {
+document.getElementById("feniHandle").addEventListener("click", function () {
+  const inputValue = getInPutNumberValue("feniInputValue");
 
-    const inputValue = getInPutNumberValue('feniInputValue')
+  if (typeof inputValue === "number" && inputValue > 0) {
+    const numberAmount = getValueInput("feniDonationAmount");
 
-    if (typeof inputValue === 'number' && inputValue > 0) {
+    const updateAmount = numberAmount + inputValue;
+    document.getElementById("feniDonationAmount").innerText = updateAmount;
 
-        const numberAmount = getValueInput('feniDonationAmount')
+    //modal functionalities
+    const myModal = document.getElementById("myModal");
+    myModal.classList.remove("hidden");
+    const closeModalBtn = document.getElementById("closeModalBtn");
+    closeModalBtn.addEventListener("click", () => {
+      myModal.classList.add("hidden");
+    });
+    // alert("Your Have Donated for Humankind");
 
-        const updateAmount = numberAmount + inputValue;
-        document.getElementById('feniDonationAmount').innerText = updateAmount
+    const numbeMyBalence = getmainBalence("mainBalence");
+    const updatemyBalence = numbeMyBalence - inputValue;
+    document.getElementById("mainBalence").innerText = updatemyBalence;
 
-        alert('Your Have Donated for Humankind')
-
-        const numbeMyBalence = getmainBalence('mainBalence')
-        const updatemyBalence = numbeMyBalence - inputValue;
-        document.getElementById('mainBalence').innerText = updatemyBalence
-
-
-
-
-        // add to history
-        const now = new Date();
-        const div = document.createElement('div')
-        div.classList.add('border-2')
-        div.classList.add('p-4')
-        div.classList.add('mt-4')
-        div.innerHTML =
-            ` ${updateAmount} Taka is Donated at Feni, Bangladesh.
+    // add to history
+    const now = new Date();
+    const div = document.createElement("div");
+    div.classList.add("border-2");
+    div.classList.add("p-4");
+    div.classList.add("mt-4");
+    div.innerHTML = ` ${updateAmount} Taka is Donated at Feni, Bangladesh.
                <br>
                <p>${now}</p>
-         `
-        const historyDiv = AddedHistory("hidtory")
-        historyDiv.appendChild(div)
-    }
-})
+         `;
+    const historyDiv = AddedHistory("hidtory");
+    historyDiv.appendChild(div);
+  }
+});
 
 // 3rd part
-document.getElementById('kotHandleClick').addEventListener('click', function () {
+document
+  .getElementById("kotHandleClick")
+  .addEventListener("click", function () {
+    const inputValue = getInPutNumberValue("kotaValue");
 
-    const inputValue = getInPutNumberValue('kotaValue')
+    if (typeof inputValue === "number" && inputValue > 0) {
+      const numberAmount = getValueInput("kotaDonationAmount");
 
+      const updateAmount = numberAmount + inputValue;
+      document.getElementById("kotaDonationAmount").innerText = updateAmount;
 
-    if (typeof inputValue === 'number' && inputValue > 0) {
+      //modal functionalities
+      const myModal = document.getElementById("myModal");
+      myModal.classList.remove("hidden");
+      const closeModalBtn = document.getElementById("closeModalBtn");
+      closeModalBtn.addEventListener("click", () => {
+        myModal.classList.add("hidden");
+      });
 
-        const numberAmount = getValueInput('kotaDonationAmount')
+      const numbeMyBalence = getmainBalence("mainBalence");
+      const updatemyBalence = numbeMyBalence - inputValue;
+      document.getElementById("mainBalence").innerText = updatemyBalence;
 
-        const updateAmount = numberAmount + inputValue;
-        document.getElementById('kotaDonationAmount').innerText = updateAmount
-
-        alert('Your Have Donated for Humankind')
-
-        const numbeMyBalence = getmainBalence('mainBalence')
-        const updatemyBalence = numbeMyBalence - inputValue;
-        document.getElementById('mainBalence').innerText = updatemyBalence
-
-        // add to history
-        const now = new Date();
-        const div = document.createElement('div')
-        div.classList.add('border-2')
-        div.classList.add('p-4')
-        div.classList.add('mt-4')
-        div.innerHTML =
-            `  ${updateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh. 
+      // add to history
+      const now = new Date();
+      const div = document.createElement("div");
+      div.classList.add("border-2");
+      div.classList.add("p-4");
+      div.classList.add("mt-4");
+      div.innerHTML = `  ${updateAmount} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh. 
             <br>
             <p>${now}</p>
-         `
-        const historyDiv = AddedHistory("hidtory")
-        historyDiv.appendChild(div)
+         `;
+      const historyDiv = AddedHistory("hidtory");
+      historyDiv.appendChild(div);
     }
-})
-
-
+  });
 
 // toggle section
 
-document.getElementById('donation-btn').addEventListener('click', function () {
-    // bg-color
-    const donationbtn = document.getElementById('donation-btn');
-    donationbtn.classList.add('bg-[#B4F461]')
+document.getElementById("donation-btn").addEventListener("click", function () {
+  // bg-color
+  const donationbtn = document.getElementById("donation-btn");
+  donationbtn.classList.add("bg-[#B4F461]");
 
-    const historybtn = document.getElementById('History-btn');
-    historybtn.classList.remove('bg-[#B4F461]')
+  const historybtn = document.getElementById("History-btn");
+  historybtn.classList.remove("bg-[#B4F461]");
 
+  // hide history section
 
-    // hide history section
+  const HistoeySection = document.getElementById("history_section");
+  HistoeySection.classList.add("hidden");
 
-    const HistoeySection = document.getElementById('history_section');
-    HistoeySection.classList.add('hidden')
+  const donationSection = document.getElementById("donation_section");
+  console.log(donationSection);
+  donationSection.classList.remove("hidden");
+});
 
-    const donationSection = document.getElementById('donation_section');
-    console.log(donationSection)
-    donationSection.classList.remove('hidden')
+document.getElementById("History-btn").addEventListener("click", function () {
+  const donationbtn = document.getElementById("donation-btn");
+  donationbtn.classList.remove("bg-[#B4F461]");
 
-})
+  const historybtn = document.getElementById("History-btn");
+  historybtn.classList.add("bg-[#B4F461]");
 
+  const HistoeySection = document.getElementById("history_section");
+  HistoeySection.classList.remove("hidden");
 
-document.getElementById('History-btn').addEventListener('click', function () {
-    const donationbtn = document.getElementById('donation-btn');
-    donationbtn.classList.remove('bg-[#B4F461]')
-
-    const historybtn = document.getElementById('History-btn');
-    historybtn.classList.add('bg-[#B4F461]')
-
-
-    const HistoeySection = document.getElementById('history_section');
-    HistoeySection.classList.remove('hidden')
-
-    const donationSection = document.getElementById('donation_section');
-    console.log(donationSection)
-    donationSection.classList.add('hidden')
-
-})
-
-
+  const donationSection = document.getElementById("donation_section");
+  console.log(donationSection);
+  donationSection.classList.add("hidden");
+});
 
 // blog section
 
-document.getElementById('blog').addEventListener('click', function () {
-    const donationSection = document.getElementById('donation_section');
-    donationSection.classList.add('hidden')
-    const HistoeySection = document.getElementById('history_section');
-    HistoeySection.classList.add('hidden')
-    const faq = document.getElementById('faq-section');
-    faq.classList.remove('hidden')
-    const btnDiv = document.getElementById('btn-div');
-    btnDiv.classList.add('hidden')
-})
+document.getElementById("blog").addEventListener("click", function () {
+  const donationSection = document.getElementById("donation_section");
+  donationSection.classList.add("hidden");
+  const HistoeySection = document.getElementById("history_section");
+  HistoeySection.classList.add("hidden");
+  const faq = document.getElementById("faq-section");
+  faq.classList.remove("hidden");
+  const btnDiv = document.getElementById("btn-div");
+  btnDiv.classList.add("hidden");
+});
 
 // back to Home
 
-document.getElementById('backTohHome').addEventListener('click', function () {
-    const donationSection = document.getElementById('donation_section');
-    donationSection.classList.remove('hidden')
-    const HistoeySection = document.getElementById('history_section');
-    HistoeySection.classList.add('hidden')
-    const faq = document.getElementById('faq-section');
-    faq.classList.add('hidden')
-    const btnDiv = document.getElementById('btn-div');
-    btnDiv.classList.remove('hidden')
-})
-
-
-
-
-
-
+document.getElementById("backTohHome").addEventListener("click", function () {
+  const donationSection = document.getElementById("donation_section");
+  donationSection.classList.remove("hidden");
+  const HistoeySection = document.getElementById("history_section");
+  HistoeySection.classList.add("hidden");
+  const faq = document.getElementById("faq-section");
+  faq.classList.add("hidden");
+  const btnDiv = document.getElementById("btn-div");
+  btnDiv.classList.remove("hidden");
+});
